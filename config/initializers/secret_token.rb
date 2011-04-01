554f8406d8681a96bb454f988aa2c204fcac2272
bitnami <kalickl@azdhs.gov>
2011-04-01 22:39:53 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '224f2a3b2daf24d8b902ddbe16a4164ac26c8225c71a7979ce876d663c8f39865a55f43b6c4e4fb651fdff9d939157e586a697a02c282ac1aab5c9ce8e68fa74'
